ee46ecdc9a0e0a1d1d7bdc566417155d959f7945
HEEEUN9812 <heeeun9812@gmail.com>
2025-04-24 23:44:57 +0900

    2025-04-24 cors 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const allowedOrigins = configService.get<string>('ALLOWED_ORIGINS').split(',');
+
+  app.enableCors({
+    origin: allowedOrigins
+  });
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
