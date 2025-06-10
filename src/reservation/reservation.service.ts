3e5ee2b7030b5a9981b1204e9368a1c52cdf5192
HEEEUN9812 <heeeun9812@gmail.com>
2025-06-10 23:13:59 +0900

    2025-06-10 예약 조회 API 구조 변경

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -5,12 +5,17 @@ import { Repository } from 'typeorm';
 import { CreateReservationDto } from './dto/reservation-create';
 import { UserInfoDto } from 'src/user/dto/user-info';
 import { STATUS } from 'src/common/enum/data-stauts.enum';
+import { InjectModel } from '@nestjs/mongoose';
+import { TravelProduct } from 'src/product/product.document';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ReservationService {
   constructor(
     @InjectRepository(Reservation)
     private reservationRepository: Repository<Reservation>,
+    @InjectModel(TravelProduct.name)
+    private productModel: Model<TravelProduct>,
   ) {}
 
   async createReservation(user: any, dto: CreateReservationDto) {
@@ -35,10 +40,31 @@ export class ReservationService {
   async getReservation(user: any) {
     const userId = user.id;
 
-    return await this.reservationRepository.find({
+    const reservations =  await this.reservationRepository.find({
       where: { userId: userId },
       order: { reservationDate: 'DESC' },
     });
+
+    const prodId = reservations.map(reservation => reservation.prodId);
+
+    const products = await this.productModel.find({_id: {$in: prodId}}).exec();
+
+    const productMap = new Map();
+    products.forEach(product => {
+      productMap.set(product._id, product);
+    });
+
+    const result = reservations.map(reservation => {
+      const {prodId, ...reservationWithoutProd} = reservation;
+      const product = productMap.get(prodId);
+
+      return {
+        ...reservationWithoutProd,
+        product: product || null
+      };
+    });
+
+    return result;
   }
 
   async cancledReservation(user: any, id: string) {
