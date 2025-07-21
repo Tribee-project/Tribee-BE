035779615c17b02bc69d20974fcf4319446aeb05
HEEEUN9812 <heeeun9812@gmail.com>
2025-07-21 22:25:22 +0900

    2025-07-21 pg 모듈 수정

--- src/config/postgresql.config.ts
+++ src/config/postgresql.config.ts
@@ -19,9 +19,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       autoLoadEntities: true,
 
       // ✅ Supabase는 SSL 사용 필수
-      // ssl: {
-      //   rejectUnauthorized: false,
-      // },   
+      ssl: false
          
     };
   }
